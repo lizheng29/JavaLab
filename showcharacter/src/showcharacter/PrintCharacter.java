9d5cf003a136cf1e11ab05e6228e056b5615d203
Tinet-lizheng <lizhengyeah@qq.com>
2016-12-23 10:06:30 +0800

    将字母编码，用*打印字母

---
+++ showcharacter/src/showcharacter/PrintCharacter.java
@@ -0,0 +1,31 @@
+package showcharacter;
+
+public class PrintCharacter {
+	
+	public static void print(int[] info){
+		for (int i = 0; i < info.length; i++) {
+			if(info[i]>0){
+				for (int j = 0; j < info[i]; j++) {
+					System.out.print("    *");
+				}
+			}else if(info[i]==0){
+				for (int j = 0; j < info[i+1]; j++) {
+					System.out.print("     ");
+				}
+				i+=1;
+			}else if(info[i]<0){
+				System.out.print("\n");
+			}
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		CharacterCode characterCode = new CharacterCode();
+		int[] code= characterCode.C;
+		print(code);
+		
+	}
+
+}
