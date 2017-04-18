0409c663ae0692e5ea06979c9f672742dbe05ca6
Tinet-lizheng <lizhengyeah@qq.com>
2017-04-18 15:06:39 +0800

    java的calendar练习

--- myTestProject/src/myTestProject/Test.java
+++ myTestProject/src/myTestProject/Test.java
@@ -1,5 +1,29 @@
 package myTestProject;
 
-public class Test {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+public class Test {
+	
+	
+	public static void Test(){
+		//获得10天前的date
+		Calendar calendar =Calendar.getInstance();
+		calendar.setTime(new Date());
+		calendar.set(Calendar.DATE, calendar.get(Calendar.DATE)-10);
+		Date date = calendar.getTime();
+		System.out.println(date.toLocaleString());
+		//格式化date为string
+		DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		String dateStr = sdf.format(date);
+		dateStr+=" 00:00:00";
+		System.out.println(dateStr);
+		
+	}
+
+	public static void main(String[] args) {
+		Test();
+	}
 }
