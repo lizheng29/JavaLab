7b48ccab2a842950d736af4540114df3ea46021d
Tinet-lizheng <lizhengyeah@qq.com>
2017-05-03 16:25:03 +0800

    探索JPA的排序，分页，流式查询，自定义查询，原生sql查询

--- playspringboot/src/main/java/com/lizheng/play/controller/PlayController.java
+++ playspringboot/src/main/java/com/lizheng/play/controller/PlayController.java
@@ -1,22 +1,21 @@
 package com.lizheng.play.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lizheng.play.model.Person;
 import com.lizheng.play.respository.PersonRepository;
@@ -31,9 +30,29 @@ public class PlayController {
 
 	
 	@GetMapping(value="/")
+	@Transactional(readOnly = true)
 	public String getPersons(Model model){
 		Person empty = new Person();
-		List<Person> persons= personRepository.findAll();
+		// 使用自定义语句方式查询并排序
+		//List<Person> persons= personRepository.findAllOrderByAgeAsc();
+		// 使用原生sql查询并排序
+		//List<Person> persons = personRepository.myfindAll();
+		// 使用Sort对象排序(单个或多个，但是同种排序)
+		//Sort sort = new Sort(Sort.Direction.ASC,"age","length");
+		//使用Order数组排序(多个)
+		Order[] orders= {new Order(Sort.Direction.ASC,"age"),new Order(Sort.Direction.ASC,"length")};
+		Sort sort = new Sort(orders);
+		List<Person> persons = personRepository.findAll(sort);
+		// 查找所有年龄小于50岁的
+		//List<Person> persons = personRepository.findByAgeLessThanEqual(50);
+		// 查询分页对象并排序
+		//Page<Person> page = personRepository.findAll(new PageRequest(0,10,Direction.ASC,"age","length"));
+		// 使用流式查询
+		try(Stream<Person> personStream = personRepository.streamAll()){
+			personStream.forEach(person -> System.out.println(person.getName()));
+			personStream.close();
+		}
+		
 		model.addAttribute("random", Math.random());
 		model.addAttribute("persons", persons);
 		model.addAttribute("emptyPerson", empty);
--- playspringboot/src/main/java/com/lizheng/play/respository/PersonRepository.java
+++ playspringboot/src/main/java/com/lizheng/play/respository/PersonRepository.java
@@ -1,9 +1,27 @@
 package com.lizheng.play.respository;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.lizheng.play.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person,Integer>{
 
+	@Query("select p from Person p order by p.age asc")
+	List<Person> findAllOrderByAgeAsc();
+	
+	List<Person> findByAgeLessThanEqual(Integer age);
+	
+	Page<Person> findAll(Pageable pageable);
+	
+	@Query(value = "select * from Person where 1=1 order by age desc", nativeQuery = true)
+	List<Person> myfindAll();
+	
+	@Query("select p from Person p order by p.age asc")
+	Stream<Person> streamAll();
 }
