78d248bcf11ff4ed9ab7a3109e3820431a25d872
Tinet-lizheng <lizhengyeah@qq.com>
2017-07-24 09:36:20 +0800

    没什么用的修改

--- javaInputStream/src/inputtest/ScannerTest.java
+++ javaInputStream/src/inputtest/ScannerTest.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 
 public class ScannerTest {
 	public static void main(String[] args) {
-		
+
 		String read = "";
-		Scanner scan = new Scanner(System.in);
-		while(true){
+		Scanner scan = new Scanner(System.in,"UTF-8");
+		while (true) {
 			System.out.print("输入Scanner数据：");
-			read = scan.nextLine();
-			System.out.println("输入了："+read);
-			if(read.equals("q")){
-				System.out.println("输入了退出符号");
-				
-				break;
+			if (scan.hasNext()) {
+				read = scan.nextLine();
+				System.out.println("输入了：" + read);
+				if (read.equals("q")) {
+					System.out.println("输入了退出符号");
+					break;
+				}
 			}
 		}
 		scan.close();
