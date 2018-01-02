d3606b217c8feb4a0b49674f42fe7eaa217424d4
Tinet-lizheng <lizhengyeah@qq.com>
2018-01-02 17:24:06 +0800

    新增eureka server 未果

--- playspringboot/src/main/java/com/lizheng/play/PlaySpringBootApplication.java
+++ playspringboot/src/main/java/com/lizheng/play/PlaySpringBootApplication.java
@@ -3,9 +3,10 @@ package com.lizheng.play;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 //@EnableWebSecurity //启用web安全
 public class PlaySpringBootApplication extends SpringBootServletInitializer{
